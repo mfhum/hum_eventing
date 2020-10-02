7399662986e200e38369d467c142de2321fc3389
schmarss <hummelmarius87@gmail.com>
2020-10-02 11:02:47 +0200

    added home component with routing

--- hum/src/app/app.module.ts
+++ hum/src/app/app.module.ts
@@ -6,8 +6,12 @@ import { AppComponent } from './app.component';
 import { KasseComponent } from './kasse/kasse.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const appRoutes: Routes = [{ path: 'kasse', component: KasseComponent }];
+const appRoutes: Routes = [
+  { path: 'kasse', component: KasseComponent },
+  { path: '', component: HomeComponent },
+];
 @NgModule({
   declarations: [AppComponent, KasseComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
