5a2796002b7f72af236f5ce60e1d663a0e17b22d
schmarss <hummelmarius87@gmail.com>
2020-10-02 09:33:50 +0200

    init

--- hum/src/app/app.module.ts
+++ hum/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KasseComponent } from './kasse/kasse.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KasseComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ hum/src/app/kasse/kasse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kasse',
+  templateUrl: './kasse.component.html',
+  styleUrls: ['./kasse.component.css']
+})
+export class KasseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
